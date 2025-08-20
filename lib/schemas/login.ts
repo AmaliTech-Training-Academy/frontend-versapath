2e3fe29045385ee9fab8a73bced17d0ff3032e86
Bob Karemera Shema <bobshema14@gmail.com>
2025-08-20 16:30:44 +0200

    VER-65: added loading state and colors to login form UI

--- lib/schemas/login.ts
+++ lib/schemas/login.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const loginSchema = z.object ({
-    email: z.email(),
-    password: z.string()
+    email: z.email().nonempty('Email required'),
+    password: z.string().nonempty('Password required')
 });
 
 export type loginInputs = z.infer<typeof loginSchema>;
