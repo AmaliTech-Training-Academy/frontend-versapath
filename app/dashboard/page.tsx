3b6b1341fae0210ed1c420049ffefbca7a0d0d09
Bob Karemera Shema <bobshema14@gmail.com>
2025-08-27 14:57:41 +0200

    refactor: cleaned up metrics section on Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,15 +2,39 @@ import { Activity, CheckCircle, UsersIcon } from "lucide-react";
 import { DashboardHeader } from "./components/header";
 import { MetricsCard } from "./components/metrics-card";
 
+const metrics = [
+    {
+        title: 'Total Users',
+        value: 0,
+        icon: UsersIcon
+    },
+    {
+        title: 'Active Learners',
+        value: 0,
+        icon: UsersIcon
+    },
+    {
+        title: 'Completed Skills',
+        value: 0,
+        icon: CheckCircle
+    },
+    {
+        title: 'Active Growth Tracks',
+        value: 0,
+        icon: Activity
+    }
+]
+
 export default function DashboardPage() {
     return (
         <>
             <DashboardHeader title="Dashboard" />
             <section className="bg-sidebar w-full grid grid-cols-4 gap-6">
-                <MetricsCard title="Total Users" value={1000} icon={UsersIcon} />
-                <MetricsCard title="Active Learners" value={900} icon={UsersIcon} />
-                <MetricsCard title="Completed Skills" value={200} icon={CheckCircle} />
-                <MetricsCard title="Active Growth Tracks" value={100} icon={Activity} />
+                {
+                    metrics.map(metric => (
+                        <MetricsCard key={metric.title} title={metric.title} value={metric.value} icon={metric.icon} />
+                    ))
+                }
             </section>
         </>
     );
