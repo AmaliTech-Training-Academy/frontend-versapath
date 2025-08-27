3d44927ba9e321c1b1d9dda95ee88631a8a752a0
Bob Karemera Shema <bobshema14@gmail.com>
2025-08-27 14:25:03 +0200

    style: Implement UI for dashboard

--- app/dashboard/components/metrics-card.tsx
+++ app/dashboard/components/metrics-card.tsx
@@ -1,8 +1,22 @@
-export const MetricsCard = ({ title, value, icon }: { title: string; value: string | number; icon: React.ReactNode; }) => {
+import { LucideProps } from "lucide-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+
+interface MetricsCardProps {
+  title: string;
+  value: string | number;
+  icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
+}
+
+export const MetricsCard = ({ title, value, icon: Icon }: MetricsCardProps) => {
   return (
-    <div className="bg-white p-4 rounded-lg shadow">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-2xl font-bold">{value}</p>
-    </div>
+    <article className="space-y-4 border rounded-lg py-4 px-5">
+      <div className="flex items-center justify-between">
+        <span className="text-gray-text-strong/70">{title}</span>
+        <div className="border border-brand-primary-text-weak rounded-lg p-4">
+          <Icon className="w-[18px] h-[18px] text-brand-primary-text" />
+        </div>
+      </div>
+      <h6 className="font-semibold text-2xl text-gray-text-strong/70">{value}</h6>
+    </article>
   );
 };
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,16 @@
+import { Activity, CheckCircle, UsersIcon } from "lucide-react";
 import { DashboardHeader } from "./components/header";
+import { MetricsCard } from "./components/metrics-card";
 
 export default function DashboardPage() {
     return (
         <>
             <DashboardHeader title="Dashboard" />
-            <section className="bg-sidebar p-3 rounded-lg  h-full">
-                
+            <section className="bg-sidebar w-full grid grid-cols-4 gap-6">
+                <MetricsCard title="Total Users" value={1000} icon={UsersIcon} />
+                <MetricsCard title="Active Learners" value={900} icon={UsersIcon} />
+                <MetricsCard title="Completed Skills" value={200} icon={CheckCircle} />
+                <MetricsCard title="Active Growth Tracks" value={100} icon={Activity} />
             </section>
         </>
     );
