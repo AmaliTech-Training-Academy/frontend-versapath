2350145675fc0e80b7a0a70369e706365012c3b5
Bob Karemera Shema <bobshema14@gmail.com>
2025-09-04 19:50:51 +0200

    VER-104: Implemented skill capsule containers and added a template roadmap timeline component

---
+++ app/dashboard/roadmap/components/capsule-box.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+import { Progress } from "@/components/ui/progress"
+import Link from "next/link"
+
+export const CapsuleBox = ({ capsule }: { capsule: { name: string, description: string, progress?: number } }) => {
+    return (
+        <div className="bg-sidebar w-[344px] rounded-md space-y-4 border border-brand-primary-text-weak">
+            <p className="font-semibold text-sm text-gray-text-strong py-1 px-2">{capsule.name}</p>
+            <p className="text-xs text-gray-text-weak px-2">{capsule.description}</p>
+            <div className="flex items-center justify-between p-2">
+                {
+                    capsule.progress === undefined && (
+                        <Link href="#" className="ml-auto text-sm text-gray-text-strong font-medium tracking-normal px-3">Learn more</Link>
+                    )
+                }
+                {
+                    capsule.progress !== undefined && (
+                        <div className="flex items-center space-x-1">
+                            <span className="text-xs text-gray-text-weak">{capsule.progress}%</span>
+                            <Progress value={capsule.progress} className="w-[200px] h-3 rounded-full bg-green-text/20 [&>div]:bg-green-text" />
+                        </div>
+                    )
+                }
+                {
+                    capsule.progress !== undefined && capsule.progress === 0 && (
+                        <Link href="#" className="text-sm text-gray-text-strong font-medium tracking-normal px-3">Start</Link>
+                    )
+                }
+                {
+                    capsule.progress !== undefined && capsule.progress > 0 && (
+                        <Link href="#">
+                            <Button>
+                                Resume
+                            </Button>
+                        </Link>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
--- app/dashboard/roadmap/page.tsx
+++ app/dashboard/roadmap/page.tsx
@@ -1,15 +1,23 @@
 import { DashboardHeader } from "../components/header";
 import { roadmap } from "@/lib/api/roadmap";
 import { GrowthTrackOverview } from "./components/growth-track-overview";
+import { CapsuleBox } from "./components/capsule-box";
 
 export default function SkillClustersPage() {
     const { name, description, progress, capsules } = roadmap;
     return (
-        <>
+        <div className="space-y-6">
             <DashboardHeader title="Roadmap" />
             <GrowthTrackOverview data={{ name, description, progress }} />
-            <section>
+            <section className="bg-[url(/images/auth-background.jpg)] bg-cover bg-no-repeat bg-bottom rounded-md">
+                <div className="bg-[#00708a]/20">
+                    {
+                        capsules.map((capsule, index) => (
+                            <CapsuleBox key={index} capsule={capsule} />
+                        ))
+                    }
+                </div>
             </section>
-        </>
+        </div>
     );
 }
