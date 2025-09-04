e388acfd7ce201d3f17e4f9559ced00c465ef1dc
Favor-star <favoureliab@gmail.com>
2025-09-04 16:23:34 +0200

    fix: fixed string errors on the register user schema on completring user registration

--- lib/schemas/register.ts
+++ lib/schemas/register.ts
@@ -4,18 +4,20 @@ export const registerSchema = z
   .object({
     email: z.email(),
     fullName: z
-      .string()
+      .string("Names are required")
       .nonempty("Full Name is required")
       .min(4, "Full Name must be at least 4 characters long"),
     password: z
-      .string()
+      .string({ error: "Password is required" })
       .nonempty("Password is required")
       .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/, {
         error:
           "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number",
       }),
-    confirmPassword: z.string(),
-    username: z.string().min(3, "Username is required"),
+    confirmPassword: z.string("Required"),
+    username: z
+      .string({ error: "Username is required" })
+      .min(3, "Username is required"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     error: "Passwords must match",
