e3246564c1c2b867834bd9a4a2ab3a208f96cdb6
Bob Karemera Shema <bobshema14@gmail.com>
2025-09-09 15:30:07 +0200

    VER-146: implemented badge list components

--- app/dashboard/components/badge-list.tsx
+++ app/dashboard/components/badge-list.tsx
@@ -1,5 +1,18 @@
+import { dashboardBadges } from "@/lib/api/badges";
+import { CardHeader } from "./card-header";
+import { BadgeCard } from "./badge-card";
+
 export const BadgeList = () => {
     return (
-        <article className="col-span-2"></article>
+        <article className="bg-sidebar rounded-md p-5 space-y-4">
+            <CardHeader title="My Badges" url="#" />
+            <div className="space-y-7">
+                {
+                    dashboardBadges.map((badge) => (
+                        <BadgeCard key={badge.id} badge={badge} />
+                    ))
+                }
+            </div>
+        </article>
     );
 }
---
+++ lib/api/badges.ts
@@ -0,0 +1,34 @@
+import icon from '@/public/images/mdi_badge.svg';
+
+export const dashboardBadges = [
+    {
+        id: 'ajkldfh98234789324',
+        title: 'React Components',
+        date: '28/04/2025',
+        tag: 'Beginner',
+        icon
+    },
+    {
+        id: 'aldfj03984209rdsanjkf',
+        title: 'React Components',
+        date: '28/04/2025',
+        tag: 'Beginner',
+        icon
+    },
+    {
+        id: 'ajosdf20394820348203',
+        title: 'React Components',
+        date: '28/04/2025',
+        tag: 'Beginner',
+        icon
+    },
+    {
+        id: 'aajsdfhi8392490234832',
+        title: 'React Components',
+        date: '28/04/2025',
+        tag: 'Beginner',
+        icon
+    }
+];
+
+export type DashBoardBadge = typeof dashboardBadges[0];
