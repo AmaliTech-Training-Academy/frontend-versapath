1573b5b33e7381400c015daacdee1c8b2a3e7f03
Bob Karemera Shema <bobshema14@gmail.com>
2025-09-09 10:27:14 +0200

    VER-146: Added a hover state to the quick action cards

--- app/dashboard/components/quick-action-card.tsx
+++ app/dashboard/components/quick-action-card.tsx
@@ -9,7 +9,7 @@ interface QuickActionCardProps {
 
 export const QuickActionCard = ({ title, description, icon: Icon }: QuickActionCardProps) => {
     return (
-        <article className="space-y-1 border border-gray-stroke-weak rounded-xl p-4">
+        <article className="space-y-1 border border-gray-stroke-weak rounded-xl p-4 cursor-pointer hover:shadow-xl">
             <div className="rounded-[99px] text-brand-secondary-stroke-weak p-2 w-fit border border-brand-primary-text bg-[#338280]">
                 <Icon className="w-[18px] h-[18px] text-brand-secondary-text" />
             </div>
