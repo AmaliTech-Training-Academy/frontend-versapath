b22608ddc552c9222b336ccae5ec283e2285272f
umbereye27 <carineumbereye7@gmail.com>
2025-09-16 14:26:16 +0200

    Add recent activity list component and integrate into dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { BadgeList } from "./components/badge-list";
 import { DashboardHeader } from "./components/header";
 import { Metrics } from "./components/metrics";
 import { QuickActions } from "./components/quick-actions";
 import { RecentActivityList } from "./components/recent-activity-list";
 import { RoadmapList } from "./components/roadmap-list";
-
 export default function DashboardPage() {
     return (
         <>
@@ -13,9 +11,9 @@ export default function DashboardPage() {
             <QuickActions />
             <section className="grid grid-cols-3 gap-6">
                 <RoadmapList />
-                <BadgeList />
+                <RecentActivityList />
             </section>
-            <RecentActivityList />
+            
         </>
     );
 }
