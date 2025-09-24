b377e72e3b6154dccc4b2fd2bf1bb7afb2901d9c
Favor-star <favoureliab@gmail.com>
2025-09-24 09:58:30 +0200

    feat: modified skill creation schema to match backend validation

--- lib/schemas/add-skill.ts
+++ lib/schemas/add-skill.ts
@@ -1,30 +1,35 @@
 import { z } from "zod";
 import { DifficultyLevels, ProfficiencyLevels } from "../types";
-import { error } from "console";
 
 export const addSkillSchema = z.object({
   name: z
-    .string({ error: "Skill name is required" })
-    .min(4, "Skill name must be at least 4 characters long"),
-  description: z
-    .string({ error: "Description is required" })
-    .min(10, "Description must be at least 10 characters long"),
-  objectives: z
-    .string({ error: "Objectives are required" })
-    .min(10, "Objectives must be at least 10 characters long"),
+    .string({ error: "Name is required" })
+    .min(3, "Skill name must have at least 3 characters")
+    .regex(/^[A-Za-z ]+$/, "Name cannot contain numbers"),
+  description: z.string({ error: "Description is required" }).optional(),
+  objectives: z.string({ error: "Objectives are required" }).optional(),
   proficiencyLevel: z.enum(ProfficiencyLevels, {
     error: "Select a proficiency level",
   }),
   categories: z
     .array(z.string(), "At least one category is required")
-    .min(1, "At least one category is required"),
-  difficulty: z.enum(DifficultyLevels, {
-    error: "Select a difficulty level",
-  }),
+    .optional(),
+  difficulty: z
+    .enum(DifficultyLevels, {
+      error: "Select a difficulty level",
+    })
+    .optional(),
   estimatedHours: z
     .string({ error: "Estimated hours is required" })
-    .regex(/^\d+$/, "Estimated hours must be a number"),
-  tags: z.array(z.string()).min(1, "At least one tag is required"),
+    .regex(/^\d+(\.\d+)?$/, "Estimated hours must be a number")
+    .transform((val) => parseFloat(val))
+    .refine((val) => val >= 1, {
+      error: "Estimated hours must be at least 1",
+    })
+    .refine((val) => val <= 1000, {
+      error: "Estimated hours must not exceed 1000",
+    }),
+  tags: z.array(z.string()).optional(),
   cover: z
     .file()
     .max(1_000_000, { error: "Max file size is 1MB." })
