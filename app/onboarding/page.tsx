5d081e2e6c4e83f7057b8e56aeef0426d12c7f50
Bob Karemera Shema <bobshema14@gmail.com>
2025-09-29 16:29:15 +0200

    Full onboarding UI implementation - no integration

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,24 +1,15 @@
-"use client";
 import Image from "next/image";
 import logo from "@/public/Logo.svg";
-import { useSession } from "next-auth/react";
+import { OnboardingForm } from "./components/onboarding-form";
 
 export default function OnboardingPage() {
-    const { data: session } = useSession();
-    const firstName = session?.user.firstName || "";
     return (
-        <main className="min-h-screen bg-[url(/images/auth-background.jpg)] bg-cover bg-no-repeat bg-bottom">
+        <main className="min-h-screen bg-[url(/images/auth-background.jpg)] bg-cover bg-no-repeat bg-bottom space-y-2">
             <section className="flex items-center gap-1 py-4 px-8">
                 <Image src={logo} alt="VersaPath logo" width={52} height={52} />
                 <span className="font-semibold text-2xl text-gray-text-strong">VersaPath</span>
             </section>
-            <section className="w-fit mx-auto rounded-xl p-6 space-y-6 bg-base-white shadow-lg">
-                <article className="space-y-2 text-center">
-                    <h5 className="font-semibold text-[32px] text-gray-text-strong">Hi {firstName}, ready to grow?</h5>
-                    <p>Select routes that interest you and we will make recommandations for you</p>
-                </article>
-                <article className="grid grid-cols-3 gap-4"></article>
-            </section>
+            <OnboardingForm />
         </main>
     );
 }
---
+++ lib/api/routes.ts
@@ -0,0 +1,34 @@
+import placeholder from "@/public/images/category-placeholder.jpg";
+
+export const routes = [
+    {
+        id: "sdfsdf",
+        title: "Frontend",
+        cover: placeholder
+    },
+    {
+        id: "dgfvfbghrte",
+        title: "Backend",
+        cover: placeholder
+    },
+    {
+        id: "awetrgfsdg",
+        title: "QA",
+        cover: placeholder
+    },
+    {
+        id: "dkjkhjkb",
+        title: "DevOps",
+        cover: placeholder
+    },
+    {
+        id: "ahjksdfhjs",
+        title: "Mobile",
+        cover: placeholder
+    },
+    {
+        id: "weriouen",
+        title: "Machine Learning",
+        cover: placeholder
+    }
+]
