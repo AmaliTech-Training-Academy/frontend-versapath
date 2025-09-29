df1cf4454a5725edc690a6c8098b567bb75cad5d
Bob Karemera Shema <bobshema14@gmail.com>
2025-09-29 18:54:34 +0200

    added requested changes

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -4,12 +4,16 @@ import { OnboardingForm } from "./components/onboarding-form";
 
 export default function OnboardingPage() {
     return (
-        <main className="min-h-screen bg-[url(/images/auth-background.jpg)] bg-cover bg-no-repeat bg-bottom space-y-2">
-            <section className="flex items-center gap-1 py-4 px-8">
+        <main className="relative min-h-screen bg-[url(/images/auth-background.jpg)] bg-cover bg-no-repeat bg-bottom space-y-2">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-base-dark-background/5" />
+            <section className="relative z-10 flex items-center gap-1 py-4 px-8">
                 <Image src={logo} alt="VersaPath logo" width={52} height={52} />
                 <span className="font-semibold text-2xl text-gray-text-strong">VersaPath</span>
             </section>
-            <OnboardingForm />
+            <div className="relative z-10">
+                <OnboardingForm />
+            </div>
         </main>
     );
 }
