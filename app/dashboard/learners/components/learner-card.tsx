d38346e47630fe41c275a25807c7da394c0eab97
Bob Karemera Shema <bobshema14@gmail.com>
2025-10-07 10:49:35 +0200

    Integrated the learners page UI for the mentor

--- app/dashboard/learners/components/learner-card.tsx
+++ app/dashboard/learners/components/learner-card.tsx
@@ -1,8 +1,9 @@
-import { Learner } from "@/lib/api/mentor-learners";
+import { MentorLearner } from "@/lib/api/use-mentor-learner";
 import clsx from "clsx";
 import Image from "next/image";
+import placeholder from "@/public/images/mary.png";
 
-export const LearnerCard = ({ learner, hover }: { learner: Learner, hover?: boolean }) => {
+export const LearnerCard = ({ learner, hover }: { learner: MentorLearner, hover?: boolean }) => {
     return (
         <div className={clsx(
             "rounded-xl border border-gray-stroke-weak bg-base-white p-5",
@@ -12,15 +13,15 @@ export const LearnerCard = ({ learner, hover }: { learner: Learner, hover?: bool
         )}>
             <div className="flex items-center gap-[11px]">
                 <Image
-                    src={learner.image}
+                    src={placeholder}
                     width={2457}
                     height={2460}
-                    alt={learner.fullName}
+                    alt={learner.firstName + " " + learner.lastName}
                     className="w-[75px] h-[75px] rounded-full object-cover"
                 />
                 <div className="space-y-2.5">
-                    <p className="font-semibold text-lg text-gray-text-strong">{learner.fullName}</p>
-                    <p className="text-sm text-gray-text-weak">{learner.role}</p>
+                    <p className="font-semibold text-lg text-gray-text-strong">{learner.firstName + " " + learner.lastName}</p>
+                    <p className="text-sm text-gray-text-weak">Learner</p>
                     <p className="text-sm text-gray-text-weak">{learner.email}</p>
                 </div>
             </div>
--- app/dashboard/learners/components/learner-list.tsx
+++ app/dashboard/learners/components/learner-list.tsx
@@ -1,16 +1,40 @@
-import { learners } from "@/lib/api/mentor-learners";
+"use client";
 import { LearnerCard } from "./learner-card";
 import { SheetWrapper } from "../../components/sheet-wrapper";
 import { LearnerContent } from "./learner-content";
+import { useMentorLearners } from "@/lib/api/use-mentor-learner";
+import { useSession } from "next-auth/react";
+import { Loader } from "lucide-react";
 
 export const LearnerList = () => {
+    const { data: session } = useSession();
+    const mentorId = session?.user.userId || "";
+    const { learners, loading, error } = useMentorLearners(mentorId);
+
+    if (loading || error) {
+        return (
+            <section className="flex items-center justify-center w-full h-96">
+                {
+                    loading && (
+                        <>
+                            <Loader className="animate-spin" />
+                            <span>Loading...</span>
+                        </>
+                    )
+                }
+
+                {error && <span>{error}</span>}
+            </section>
+        );
+    }
+
     return (
         <article className="grid grid-cols-2 gap-4">
             {
                 learners.map(learner => (
                     <SheetWrapper
-                        key={learner.id}
-                        trigger={<LearnerCard key={learner.id} learner={learner} hover />}
+                        key={learner.email + learner.firstName}
+                        trigger={<LearnerCard learner={learner} hover />}
                         headerTitle="Learner Details"
                     >
                         <LearnerContent learner={learner} />
