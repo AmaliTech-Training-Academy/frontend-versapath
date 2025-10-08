4403d523ecb680d73ca1253b8e10fe39714bb783
Bob Karemera Shema <bobshema14@gmail.com>
2025-10-08 13:02:53 +0200

    Integrated dashboard components and rendered them dynamically

--- app/dashboard/components/roadmap-list.tsx
+++ app/dashboard/components/roadmap-list.tsx
@@ -1,18 +1,34 @@
-import { dashboardRoadmaps } from "@/lib/api/roadmap";
+"use client";
+
 import { CardHeader } from "./card-header";
 import { RoadmapCard } from "./roadmap-card";
+import { useAllTracks } from "@/lib/api/use-track";
+import { useSession } from "next-auth/react";
+import { Loader } from "lucide-react";
 
 export const RoadmapList = () => {
+    const { data: session } = useSession();
+    const id = session?.user.userId ?? "";
+    const { tracks, loading, error } = useAllTracks(id);
     return (
         <article className="rounded-xl p-5 space-y-4 bg-sidebar col-span-2">
             <CardHeader title="My Learning Roadmaps" url="#" />
-            <div className="space-y-2">
-                {
-                    dashboardRoadmaps.map((roadmap) => (
-                        <RoadmapCard key={roadmap.id} roadmap={roadmap} />
-                    ))
-                }
-            </div>
+            {
+                (loading || error || !tracks) ? (
+                    <div className="flex items-center justify-center gap-2 text-muted-foreground">
+                        <Loader className="animate-spin" />
+                        <span>Loading...</span>
+                    </div>
+                ) : (
+                    <div className="space-y-2">
+                        {
+                            tracks.map((track) => (
+                                <RoadmapCard key={track.trackId} track={track} />
+                            ))
+                        }
+                    </div>
+                )
+            }
         </article>
     );
 }
--- lib/api/use-track.ts
+++ lib/api/use-track.ts
@@ -6,11 +6,22 @@ import { apiRequest } from "./api-request";
 
 const trackFetcher = (url: string) => apiRequest<MyTrack[]>(url, "GET");
 
-export function useTrack() {
-    const { data, error, isLoading, mutate } = useSWR('/learner/my-tracks', trackFetcher);
+export function useTrack(learnerId: string) {
+    const { data, error, isLoading, mutate } = useSWR(`/learner/my-tracks/${learnerId}`, trackFetcher);
 
     return {
         track: data?.data?.at(0) ?? undefined,
+        loading: isLoading,
+        error: error ? (error as Error).message : null,
+        revalidateRoadmap: () => mutate(),
+    };
+}
+
+export function useAllTracks(learnerId: string) {
+    const { data, error, isLoading, mutate } = useSWR(`/learner/my-tracks/${learnerId}`, trackFetcher);
+
+    return {
+        tracks: data?.data ?? undefined,
         loading: isLoading,
         error: error ? (error as Error).message : null,
         revalidateRoadmap: () => mutate(),
