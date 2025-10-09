22230cc8b5df13961e37fdeb583f2c66ba1a769d
umbereye27 <carineumbereye7@gmail.com>
2025-10-09 10:37:59 +0200

    new change

--- app/dashboard/badges/page.tsx
+++ app/dashboard/badges/page.tsx
@@ -1,20 +1,5 @@
-// // import { BadgesList } from "@/components/dashboard/badges-list"
 
 import { BadgesList } from "./components/badges-list";
-
-// import { DashboardHeader } from "../components/header";
-// import { BadgeDetail } from "./components/badge-detail";
-
-// export default function BadgesPage() {
-//   return (
-//     <div className="p-6">
-//       <DashboardHeader title="Badges & Achievement"/>
-//        <BadgeDetail />
-//     </div>
-//   );
-// }
-
-// import { BadgesList } from "@/components/dashboard/badges-list"
 
 export default function BadgesPage() {
   return (
