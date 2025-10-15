236bb98aa600b34e120696f378c2c155f318771e
Bob Karemera Shema <bobshema14@gmail.com>
2025-10-15 16:34:41 +0200

    Added Learner Card unit tests

--- __tests__/assessments/assessment-card.test.tsx
+++ __tests__/assessments/assessment-card.test.tsx
@@ -1,4 +1,3 @@
-// __tests__/assessment/assessment-card.test.tsx
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
